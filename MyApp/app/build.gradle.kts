b42db4d36adb74001a4662cefd472cff672e491a
SANDY <nnal0256@naver.com>
2024-04-07 14:27:50 +0900

    [Add] detekt

--- MyApp/app/build.gradle.kts
+++ MyApp/app/build.gradle.kts
@@ -1,9 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 android {
@@ -61,6 +64,29 @@ ktlint {
         reporter(ReporterType.CHECKSTYLE)
         reporter(ReporterType.SARIF)
     }
+}
+
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = false // activate all available (even unstable) rules.
+    config.setFrom("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true) // observe findings in your browser with structure and code snippets
+        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+        md.required.set(true) // simple Markdown format
+    }
+}
+
+// Groovy DSL
+tasks.withType(Detekt::class.java).configureEach {
+    jvmTarget = "1.8"
+}
+tasks.withType(DetektCreateBaselineTask::class.java).configureEach {
+    jvmTarget = "1.8"
 }
 
 dependencies {
